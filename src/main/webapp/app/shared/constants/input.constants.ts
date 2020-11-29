5546cb55233035ad77234c50141aafc6f8cb0fe7
thinhpham5295 <ptxthinh@gmail.com>
2020-11-29 11:28:42 +0700

    Update register user

--- src/main/webapp/app/shared/constants/input.constants.ts
+++ src/main/webapp/app/shared/constants/input.constants.ts
@@ -2,4 +2,3 @@ export const DATE_FORMAT = 'YYYY-MM-DD';
 export const PHONE_PATTERN = '\\d{3}[-]\\d{3}[-]\\d{4}';
 export const PHONE_MASK_PATTERN_EX = [/\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
 export const DATE_PATTERN = 'MM/DD/YYYY';
-export const IMAGE_UPLOAD_URL = '\\src\\main\\webapp\\content\\images\\upload\\';
